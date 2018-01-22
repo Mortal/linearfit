27be59aec1f1940fe94c3adf240d65af8e4344cd
Mathias Rav <m@git.strova.dk>
2018-01-22 19:14:00 +0100

    Add weights

--- linearfit.py
+++ linearfit.py
@@ -7,16 +7,16 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--seed', type=int, default=42)
 
 
-def l1(params, data, noise_labels):
+def l1(params, data, weights, labels):
     prediction = np.dot(data, params.reshape(-1, 1))
-    dist = prediction - noise_labels
-    return np.abs(dist).mean()
+    dist = prediction - labels
+    return (weights * np.abs(dist)).sum()
 
 
-def l2(params, data, noise_labels):
+def l2(params, data, weights, labels):
     prediction = np.dot(data, params.reshape(-1, 1))
-    dist = prediction - noise_labels
-    return (dist**2).mean()
+    dist = prediction - labels
+    return (weights * dist**2).sum()
 
 
 def main():
@@ -25,6 +25,8 @@ def main():
     n = 100
     d = 5
     data = np.random.random((n, d))
+    weights = np.random.random(n) + 1
+    weights /= weights.sum()
     real_params = np.random.random((d, 1))
     real_labels = np.dot(data, real_params)
     assert real_labels.shape == (n, 1)
@@ -33,22 +35,30 @@ def main():
     assert noise_labels.shape == (n, 1)
 
     initial_params = np.zeros(d)
-    res = minimize(l1, initial_params, args=(data, noise_labels))
-    params_l2, _residuals, _rank, _s = np.linalg.lstsq(data, noise_labels, rcond=None)
+    res = minimize(l1, initial_params, args=(data, weights, noise_labels))
+    params_l2, _residuals, _rank, _s = np.linalg.lstsq(
+        data * weights.reshape(-1, 1), noise_labels * weights.reshape(-1, 1),
+        rcond=None)
     print(res)
     params_l1 = res.x
+    l1_l1 = l1(params_l1, data, weights, noise_labels)
+    l2_l1 = l1(params_l2, data, weights, noise_labels)
+    l1_l2 = l2(params_l1, data, weights, noise_labels)
+    l2_l2 = l2(params_l2, data, weights, noise_labels)
     print('')
     print('L1-param:', params_l1)
     print('L2-param:', params_l2.reshape(-1))
     print('Real param:', real_params.reshape(-1))
     print('')
-    print('L1-param L1-score:', l1(params_l1, data, noise_labels))
-    print('L2-param L1-score:', l1(params_l2, data, noise_labels))
-    print('Real L1-score:', l1(real_params, data, noise_labels))
+    print('L1-param L1-score:', l1_l1)
+    print('L2-param L1-score:', l2_l1)
+    print('Real L1-score:', l1(real_params, data, weights, noise_labels))
     print('')
-    print('L1-param L2-score:', l2(params_l1, data, noise_labels))
-    print('L2-param L2-score:', l2(params_l2, data, noise_labels))
-    print('Real L2-score:', l2(real_params, data, noise_labels))
+    print('L1-param L2-score:', l1_l2)
+    print('L2-param L2-score:', l2_l2)
+    print('Real L2-score:', l2(real_params, data, weights, noise_labels))
+    assert l1_l1 < l2_l1
+    assert l2_l2 < l1_l2
 
 
 if __name__ == '__main__':
